1279ec0ffb3b81daec989856992810e28e461351
Laurent David <lmedavid@gmail.com>
2020-01-13 12:48:49 +0100

    Rectify filter version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_competvetsuivi';
 $plugin->release = '1.0.0';
-$plugin->version = 2019101110;
+$plugin->version = 2019101120;
 $plugin->requires = 2018051700;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_RC;
