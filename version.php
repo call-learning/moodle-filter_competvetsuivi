e7a22ffbca45280b04cc90e22efb3594c18079f2
Laurent David <lmedavid@gmail.com>
2020-01-30 09:43:32 +0100

    Fix layout issue in lambda theme course by restricting the global width

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_competvetsuivi';
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
 $plugin->version = 2019101120;
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_RC;
